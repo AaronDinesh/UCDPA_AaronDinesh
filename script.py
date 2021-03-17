309df675ccacfb830fbf2a736453af743bf59db6
Aaron Dinesh <aarondinesh2002@gmail.com>
2021-03-17 14:27:53 +0000

    -Tweaked the dataset and corrected an inncorrect value

--- script.py
+++ script.py
@@ -15,16 +15,19 @@ path = 'TestCleaned.csv'
 
 
 def graphing(path, rotation, horizontal_align, x_column, y_column):
+    # Reading the Cleaned Dataset
     dataset = pd.read_csv(path, parse_dates=True)
     length = dataset[x_column].count()
+
+    # Calculating max y value
     max_y = 0
     for x in dataset['Price']:
         if x > max_y:
             max_y = x
-            print(max_y)
         else:
             pass
 
+    # Graphing part of the dataset using the x-column and the y-column parameters passed in
     x_range = np.arange(0, length, 389)
     y_range = np.arange(0, round(max_y)+1700, 1700)
     plt.plot(dataset[x_column], dataset[y_column], marker="o", markersize=0.1)
@@ -37,4 +40,8 @@ def graphing(path, rotation, horizontal_align, x_column, y_column):
     plt.show()
 
 
+def simulator():
+
+
 graphing(path, 45, 'right', 'Timestamp', 'Price')
+simulator()
